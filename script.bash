5c340ef6ed4441200d65e7ca183bd250b3514099
Jesse Ardonne <Jesse.Ardonne@gmail.com>
2015-09-17 23:56:48 -0500

    Big Update
    Added initial check for jamie0.jpg comparison
    
    Added logic to pull picture from website, and compare to other to make sure it doesn't get overloaded with same photos.
    
    Possible later todo: check all photos in a for loop to prevent the same one from being put in.

--- script.bash
+++ script.bash
@@ -1,13 +1,36 @@
 #!/bin/bash
-
 #slideshow script
 
 cd /home/pi/pics/
 
-#sleep 15
+#needed for starting first run at 1
+touch jamie0.jpg
+
+experiment () {
+        while true; do
+                #download picture
+                sudo wget http://jesseardonne.com/images/jamie.jpg
+                #transfer to unique file name using index
+                mv jamie.jpg jamie$i.jpg
+                #compare pictures
+                if diff jamie$i.jpg jamie$((i-1)).jpg >/dev/null ; then
+                        echo SAME
+                        rm jamie$i.jpg
+                else
+                        echo DIFF
+                        ((i++))
+                        echo $i > i.txt
+                fi
+                sleep 10
+         done
+}
+
+if [ ! -f /home/pi/pics/i.txt ]
+         then
+                #this is only for the first time it's ever run.
+                echo 1 > i.txt
+fi
 
 export DISPLAY=:0.0
-
-qiv -f -R -s -r -d 10 -t -i -m -T /home/pi/pics/*.jpg
-
-# Originally pulled a picture from my website to add but the Pi had an unreliable internet connection
+i=$(< i.txt)
+qiv -f -R -t -i -m -T /home/pi/pics/jamie.jpg & experiment
